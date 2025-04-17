3f6b760e0f50c325618b9aed6044e703d2f90caa
AngievR05 <159420956+AngievR05@users.noreply.github.com>
2025-04-17 10:46:30 +0200

    spelling updated

--- the-drunken-giraffe/src/App.js
+++ the-drunken-giraffe/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowerserRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
