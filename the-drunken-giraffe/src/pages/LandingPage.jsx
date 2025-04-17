a467c02b667155e166e9fb9c6f99679053cbd324
xanderMMV <241322@virtualwindow.co.za>
2025-04-17 11:24:32 +0200

    added landing nav

--- the-drunken-giraffe/src/pages/LandingPage.jsx
+++ the-drunken-giraffe/src/pages/LandingPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import '../styles/LandingPage.css';
 import '../index.css';
+import WhiteLogo from '../assets/Logo_White_No-Name.svg'
 
 function LandingPage() {
     return (
         <div className='landingDiv'>
+            <nav>
             <h1>The Drunken Giraffe</h1>
+            </nav>
+            
         </div>
     )
 }
