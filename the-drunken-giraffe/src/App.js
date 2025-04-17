fecd161ab2116bbe3b1b6e946b1a73e1798317ce
AngievR05 <159420956+AngievR05@users.noreply.github.com>
2025-04-17 10:41:10 +0200

    added pages to app.js

--- the-drunken-giraffe/src/App.js
+++ the-drunken-giraffe/src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import {BrowerserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </Router>
   );
